73745ef7934681fd46776489033065ec238383cb
Joseph Arhar <josepharhar@gmail.com>
2018-03-10 15:49:54 -0800

    finally got checksum right

--- tcp.cc
+++ tcp.cc
@@ -17,13 +17,12 @@
 
 int main(int argc, char** argv) {
   addrinfo hints, *res = 0;
-  memset(&hints, 0, sizeof(hints));
+  memset(&hints, 0, sizeof(addrinfo));
   /*hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   getaddrinfo("www.example.com", "3490", &hints, &res);*/
-  // int getaddrinfo_retval = getaddrinfo("192.168.248.1", "51371", &hints,
-  // &res);
-  int getaddrinfo_retval = getaddrinfo("127.0.0.1", "48880", &hints, &res);
+  int getaddrinfo_retval =
+      getaddrinfo("192.168.248.130", "51371", &hints, &res);
   if (getaddrinfo_retval) {
     printf("getaddrinfo() returned %d, gai_strerror(): %s\n",
            getaddrinfo_retval, gai_strerror(getaddrinfo_retval));
@@ -44,29 +43,52 @@ int main(int argc, char** argv) {
 
   printf("writing to socket\n");
 
-  uint16_t tcp_length = sizeof(TCP);
+  /*const char* payload = "hello_world";
+  int payload_length = strlen(payload);*/
+  const char* payload = "";
+  int payload_length = 0;
 
-  TCPPseudoHeader* pseudo_header =
-      (TCPPseudoHeader*)calloc(1, sizeof(TCPPseudoHeader) + tcp_length);
+  // includes data after tcp header
+  uint16_t tcp_length = sizeof(TCP) + payload_length;
+  uint16_t tcp_pseudo_length = sizeof(TCPPseudoHeader) + tcp_length;
+
+  TCPPseudoHeader* pseudo_header = (TCPPseudoHeader*)malloc(tcp_pseudo_length);
+  memset(pseudo_header, 0, tcp_pseudo_length);
+
   pseudo_header->src_ip[0] = 192;
   pseudo_header->src_ip[1] = 168;
   pseudo_header->src_ip[2] = 248;
-  pseudo_header->src_ip[3] = 10;
+  pseudo_header->src_ip[3] = 1;
   pseudo_header->dest_ip[0] = 192;
   pseudo_header->dest_ip[1] = 168;
   pseudo_header->dest_ip[2] = 248;
-  pseudo_header->dest_ip[3] = 1;
+  pseudo_header->dest_ip[3] = 130;
+  pseudo_header->reserved = 0;
   pseudo_header->protocol = IP_PROTOCOL_TCP;
   pseudo_header->SetTcpLength(tcp_length);
 
-  TCP* tcp = (TCP*)pseudo_header + 1;
+  TCP* tcp = (TCP*)(pseudo_header + 1);
   tcp->SetSrcPort(48881);
   tcp->SetDestPort(48880);
   tcp->SetSeq(0xd2113773);
-  tcp->data_offset = sizeof(tcp) / 4;
+  // data_offset must increase if using tcp options
+  tcp->data_offset = sizeof(TCP) / 4;
   tcp->syn = 1;
   tcp->SetWindowSize(29200);
-  tcp->checksum = in_cksum((short unsigned int*)pseudo_header, tcp_length);
+
+  memcpy(tcp + 1, payload, payload_length);
+
+  /*uint8_t* asdf = (uint8_t*)pseudo_header;
+  for (unsigned i = 0; i < tcp_pseudo_length; i++) {
+    printf("%02X", (int)asdf[i]);
+    if ((i + 1) % 8 == 0) {
+      printf(" ");
+    }
+    if ((i + 1) % 16 == 0) {
+      printf("\n");
+    }
+  }*/
+  tcp->checksum = in_cksum((short unsigned*)pseudo_header, tcp_pseudo_length);
 
   printf("tcp_length to write: %d\n", tcp_length);
   int bytes_written = write(fd, tcp, tcp_length);
