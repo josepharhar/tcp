2366a8c9f988b1df490e5e391227457d027e5bdd
Joseph Arhar <josepharhar@gmail.com>
2018-03-11 20:42:09 -0700

    stated making class

--- tcp.cc
+++ tcp.cc
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <errno.h>
 #include <netdb.h>
+#include <assert.h>
 
 //#include <linux/ip_fw.h>
 //#include <libiptc/libiptc.h>
@@ -26,34 +27,139 @@ static int socket_fd = -1;
 static int read_socket = -1;
 static uint8_t buffer[BUFFER_SIZE];
 
-static void ReceivedTCP(TCP* tcp) {}
+class TCPClient {
+ public:
+  // https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Tcp_state_diagram_fixed_new.svg/1280px-Tcp_state_diagram_fixed_new.svg.png
+  enum State {
+    kUninitialized,
+    kClosed,
+    kSynSent,
+    kEstablished,
+  };
+
+  TCPClient() : state_(kUninitialized), {
+    // TODO make send_seq_ random
+  }
+
+  void Start(int send_socket, uint8_t* my_ip, uint8_t* other_ip) {
+    SetState(kClosed);
+    send_socket_ = send_socket;
+    memcpy(my_ip_, my_ip, 4);
+    memcpy(other_ip_, other_ip, 4);
+
+    const char* payload = "";
+    int payload_length = 0;
+
+    // includes data after tcp header
+    uint16_t tcp_length = sizeof(TCP) + payload_length;
+    uint16_t tcp_pseudo_length = sizeof(TCPPseudoHeader) + tcp_length;
+
+    TCPPseudoHeader* pseudo_header =
+        (TCPPseudoHeader*)malloc(tcp_pseudo_length);
+    memset(pseudo_header, 0, tcp_pseudo_length);
+
+    memcpy(pseudo_header->src_ip, my_ip_, 4);
+    memcpy(pseudo_header->dest_ip, other_ip_, 4);
+    pseudo_header->reserved = 0;
+    pseudo_header->protocol = IP_PROTOCOL_TCP;
+    pseudo_header->SetTcpLength(tcp_length);
+
+    TCP* tcp = (TCP*)(pseudo_header + 1);
+    tcp->SetSrcPort(48881);
+    tcp->SetDestPort(48880);
+    tcp->SetSeq(my_seq_);
+    // data_offset must increase if using tcp options
+    tcp->data_offset = sizeof(TCP) / 4;
+    tcp->syn = 1;
+    tcp->SetWindowSize(29200);
+
+    memcpy(tcp + 1, payload, payload_length);
+
+    tcp->checksum = in_cksum((short unsigned*)pseudo_header, tcp_pseudo_length);
+
+    printf("tcp_length to write: %d\n", tcp_length);
+    int bytes_written = write(socket_fd, tcp, tcp_length);
+    free(pseudo_header);
+    printf("write() returned %d\n", bytes_written);
+
+    SetState(kSynSent);
+  }
+
+  void Receive(TCP* tcp) {
+    printf("received tcp\n");
+    printf("  fin: %d, syn: %d, rst: %d, psh: %d\n", tcp->fin, tcp->syn,
+           tcp->rst, tcp->psh);
+    printf("  ack: %d, urg: %d, ece: %d, cwr: %d\n", tcp->ack, tcp->urg,
+           tcp->ece, tcp->cwr);
+
+    switch (state_) {
+      case kClosed:
+        printf("  state is kClosed. nani!?\n");
+        break;
+      case kSynSent:
+        // packet coming back should be SYN-ACK
+        break;
+      case kEstablished:
+        // TODO
+        break;
+    }
+  }
+
+ private:
+  State state_;
+  uint32_t my_seq_;
+  uint32_t other_seq_;
+  int send_socket_;
+
+  uint8_t my_ip_[4];
+  uint8_t other_ip_[4];
+
+  void SetState(State new_state) {
+    switch (new_state) {
+      case kUninitialized:
+        assert(false);
+        break;
+      case kClosed:
+        break;
+      case kSynSent:
+        assert(state_ == kClosed);
+        break;
+      case kEstablished:
+        assert(state_ == kSynSent);
+        break;
+    }
+    state_ = new_state;
+  }
+};
+
+TCPClient tcp_client;
 
 static void ReadFromSocket(int socket) {
   memset(buffer, 0, BUFFER_SIZE);
-  int bytes_read = read(socket, buffer, BUFFER_SIZE);
-  printf("read %d bytes from socket\n", bytes_read);
+  /*int bytes_read = */ read(socket, buffer, BUFFER_SIZE);
+  // printf("read %d bytes from socket\n", bytes_read);
 
   Ethernet* ethernet = (Ethernet*)buffer;
-  printf("  ethernet dest: %s\n", ethernet->DestToString().c_str());
-  printf("  ethernet src: %s\n", ethernet->SrcToString().c_str());
-  printf("  ethernet type: %s\n", ethernet->TypeToString().c_str());
+  // printf("  ethernet dest: %s\n", ethernet->DestToString().c_str());
+  // printf("  ethernet src: %s\n", ethernet->SrcToString().c_str());
+  // printf("  ethernet type: %s\n", ethernet->TypeToString().c_str());
 
   if (ethernet->GetType() == ETHERTYPE_IP) {
     IP* ip = (IP*)(ethernet + 1);
-    printf("  ip->version: %d\n", ip->version);
-    printf("  ip->length: %d\n", ip->length);
-    printf("  ip->GetTotalLength(): %d\n", ip->GetTotalLength());
-    printf("  ip src: %s\n", IPToString(ip->source).c_str());
-    printf("  ip dest: %s\n", IPToString(ip->destination).c_str());
-    printf("  ip->protocol: %d\n", ip->protocol);
+    // printf("  ip->version: %d\n", ip->version);
+    // printf("  ip->length: %d\n", ip->length);
+    // printf("  ip->GetTotalLength(): %d\n", ip->GetTotalLength());
+    // printf("  ip src: %s\n", IPToString(ip->source).c_str());
+    // printf("  ip dest: %s\n", IPToString(ip->destination).c_str());
+    // printf("  ip->protocol: %d\n", ip->protocol);
 
     if (ip->protocol == PROTOCOL_TCP) {
       TCP* tcp = (TCP*)(ip + 1);
-      printf("  tcp src port: %d\n", tcp->GetSrcPort());
-      printf("  tcp dest port: %d\n", tcp->GetDestPort());
+      // printf("  tcp src port: %d\n", tcp->GetSrcPort());
+      // printf("  tcp dest port: %d\n", tcp->GetDestPort());
 
       if (tcp->GetSrcPort() == DEST_PORT && tcp->GetDestPort() == SRC_PORT) {
-        ReceivedTCP(tcp);
+        tcp_client.Receive(tcp);
       }
     }
   }
@@ -95,7 +201,7 @@ int main(int argc, char** argv) {
 
   /*const char* payload = "hello_world";
   int payload_length = strlen(payload);*/
-  const char* payload = "";
+  /*const char* payload = "";
   int payload_length = 0;
 
   // includes data after tcp header
@@ -133,7 +239,21 @@ int main(int argc, char** argv) {
   printf("tcp_length to write: %d\n", tcp_length);
   int bytes_written = write(socket_fd, tcp, tcp_length);
   free(pseudo_header);
-  printf("write() returned %d\n", bytes_written);
+  printf("write() returned %d\n", bytes_written);*/
+
+  uint8_t my_ip[4];
+  my_ip[0] = 192;
+  my_ip[1] = 168;
+  my_ip[2] = 248;
+  my_ip[3] = 1;
+  uint8_t other_ip[4];
+  other_ip[0] = 192;
+  other_ip[1] = 168;
+  other_ip[2] = 248;
+  other_ip[3] = 130;
+
+  printf("starting client\n");
+  tcp_client.Start(socket_fd, my_ip, other_ip);
 
   while (1) {
     ReadFromSocket(read_socket);
