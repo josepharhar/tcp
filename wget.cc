4e0910761f9d0e0822743f26940f8b1a10c119f8
Joseph Arhar <josepharhar@gmail.com>
2018-03-15 08:24:08 -0700

    changed a few things

--- wget.cc
+++ wget.cc
@@ -5,43 +5,44 @@
 
 #include "libtcp.h"
 
-unsigned int ip_addr[4];
-uint32_t port = 0;
 char path_buffer[256];
 static int libtcp_socket = -1;
 char data_buffer[65535] = {0};
 uint32_t current_position = 0;
 
 static void LoopFunction(const void* buffer, int buffer_length) {
-  char* payload = (char*)calloc(1, buffer_length + 1);
   memcpy(data_buffer + current_position, buffer, buffer_length);
   current_position += buffer_length;
+
+  char* payload = (char*)calloc(1, buffer_length + 1);
   memcpy(payload, buffer, buffer_length);
   printf("%d byte payload:\n%s\n", buffer_length, payload);
 }
 
 int main(int argc, char** argv) {
-  uint8_t my_ip[4] = {192, 168, 56, 101};
-  uint8_t dest_ip[4] = {192, 168, 56, 1};
-  // uint8_t dest_ip[4] = {0};
+  uint8_t my_ip[4] = {192, 168, 248, 130};
+
+  uint8_t dest_ip[4] = {0, 0, 0, 0};
   uint16_t my_port = 48881;
-  uint16_t dest_port = 48880;
-  FILE *fp;
-  
-  
+  uint16_t dest_port = 0;
+
+  FILE* fp;
+
   if (argc != 2) {
     printf("Usage: wget [URL]\n");
     exit(-1);
   }
-  
-  sscanf(argv[1], "%u.%u.%u.%u:%d%s\n", &ip_addr[0], &ip_addr[1], &ip_addr[2], &ip_addr[3], &port, path_buffer);
-  printf("HERE: %u.%u.%u.%u:%d%s\n", ip_addr[0], ip_addr[1], ip_addr[2], ip_addr[3], port, path_buffer);
-  // dest_ip[0] = (uint8_t) ip_addr[0];
-  // dest_ip[1] = (uint8_t) ip_addr[1];
-  // dest_ip[2] = (uint8_t) ip_addr[2];
-  // dest_ip[3] = (uint8_t) ip_addr[3];
 
-  // uint8_t dest_ip[4] = {0};
+  unsigned temp_ip_addr[4] = {0, 0, 0, 0};
+  sscanf(argv[1], "%u.%u.%u.%u:%hu%s\n", &temp_ip_addr[0], &temp_ip_addr[1],
+         &temp_ip_addr[2], &temp_ip_addr[3], &dest_port, path_buffer);
+  printf("HERE: %u.%u.%u.%u:%hu%s\n", temp_ip_addr[0], temp_ip_addr[1],
+         temp_ip_addr[2], temp_ip_addr[3], dest_port, path_buffer);
+  dest_ip[0] = (uint8_t)temp_ip_addr[0];
+  dest_ip[1] = (uint8_t)temp_ip_addr[1];
+  dest_ip[2] = (uint8_t)temp_ip_addr[2];
+  dest_ip[3] = (uint8_t)temp_ip_addr[3];
+
   char dest_ip_string[50];
   snprintf(dest_ip_string, 50, "%d.%d.%d.%d", (int)dest_ip[0], (int)dest_ip[1],
            (int)dest_ip[2], (int)dest_ip[3]);
@@ -56,7 +57,7 @@ int main(int argc, char** argv) {
   // const char* http_request = "GET /json/implemented.json\n\n";
   const char* http_method = "GET";
   char buffer[256] = {0};
-  std::string path = (std::string) path_buffer;
+  std::string path = (std::string)path_buffer;
   size_t found = path.find_last_of("/\\");
   fp = fopen(path.substr(found + 1).c_str(), "w+");
   sprintf(buffer, "%s %s\n\n", http_method, path_buffer);
